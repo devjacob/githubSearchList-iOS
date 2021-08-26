ae974481b6e763a4c3dc773d947f56640b2f3700
jacob <jacob@watcha.com>
2021-08-26 20:53:07 +0900

    검색 화면에 필요한 파일 추가 및 기본 화면 구현

---
+++ GithubSearchList/Model/Repository.swift
@@ -0,0 +1,12 @@
+//
+//  Repository.swift
+//  GithubSearchList
+//
+//  Created by jacob on 2021/08/26.
+//
+
+import Foundation
+
+struct Repository {
+    
+}
---
+++ GithubSearchList/Scenes/Search/SearchViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  SearchViewModel.swift
+//  GithubSearchList
+//
+//  Created by jacob on 2021/08/26.
+//
+
+import Foundation
+
+class SearchViewModel {
+    var repositorys: [Repository] = Array()
+}
