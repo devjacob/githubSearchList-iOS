cc70e3c2c6d0bc8e283c8f0bb9dac86fd76290f0
jacob <jacob@watcha.com>
2021-08-27 00:08:20 +0900

    repositoryCell 에 fullname, description 나오도록 구현

--- GithubSearchList/Cell/RepositoryCell.swift
+++ GithubSearchList/Cell/RepositoryCell.swift
@@ -9,5 +9,25 @@ import Foundation
 import UIKit
 
 class RepositoryCell: UICollectionViewCell {
+    // fullname
+    // Owner name
+    // itemPrivate
+    // itemDescription
 
+    @IBOutlet weak var fullnameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+
+    func bind(_ item: Item) {
+        print(item)
+        fullnameLabel.text = item.fullName
+        descriptionLabel.text = item.itemDescription
+    }
+
+    func heightFor(_ width: CGFloat) -> CGFloat {
+        frame.size = CGSize(width: width, height: frame.size.height)
+        layoutIfNeeded()
+
+        let size = contentView.systemLayoutSizeFitting(frame.size, withHorizontalFittingPriority: .required, verticalFittingPriority: .fittingSizeLevel)
+        return size.height
+    }
 }
